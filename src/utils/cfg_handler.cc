05d4d516ab326be0b155dd7ab76cb5b770013a5a
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-06-13 13:35:07 +0200

    handling kafka configs params with libconfig, the efficient way (1) ...

--- src/utils/cfg_handler.cc
+++ src/utils/cfg_handler.cc
@@ -13,37 +13,38 @@ std::unique_ptr<libconfig::Config> kafka_params(new libconfig::Config());
 
 KafkaCfgHandler::KafkaCfgHandler()
 {
-    lookup_kafka_parameters(
-    "/home/tzhcusa1/Projects/mdt-dialout-collector/configs/kafka_producer.cfg");
+    if (lookup_kafka_parameters(
+"/home/tzhcusa1/Projects/mdt-dialout-collector/configs/kafka_producer.cfg")) {
 
-    libconfig::Setting& topic_ =
-        kafka_params->lookup("topic");
-    libconfig::Setting& bootstrap_servers_ =
-        kafka_params->lookup("bootstrap_servers");
-    libconfig::Setting& enable_idempotence_ =
-        kafka_params->lookup("enable_idempotence");
-    libconfig::Setting& client_id_ =
-        kafka_params->lookup("client_id");
-    libconfig::Setting& security_protocol_ =
-        kafka_params->lookup("security_protocol");
-    libconfig::Setting& ssl_key_location_ =
-        kafka_params->lookup("ssl_key_location");
-    libconfig::Setting& ssl_certificate_location_ =
-        kafka_params->lookup("ssl_certificate_location");
-    libconfig::Setting& ssl_ca_location_ =
-        kafka_params->lookup("ssl_ca_location");
-    libconfig::Setting& log_level_ =
-        kafka_params->lookup("log_level");
+        libconfig::Setting& topic_ =
+            kafka_params->lookup("topic");
+        libconfig::Setting& bootstrap_servers_ =
+            kafka_params->lookup("bootstrap_servers");
+        libconfig::Setting& enable_idempotence_ =
+            kafka_params->lookup("enable_idempotence");
+        libconfig::Setting& client_id_ =
+            kafka_params->lookup("client_id");
+        libconfig::Setting& security_protocol_ =
+            kafka_params->lookup("security_protocol");
+        libconfig::Setting& ssl_key_location_ =
+            kafka_params->lookup("ssl_key_location");
+        libconfig::Setting& ssl_certificate_location_ =
+            kafka_params->lookup("ssl_certificate_location");
+        libconfig::Setting& ssl_ca_location_ =
+            kafka_params->lookup("ssl_ca_location");
+        libconfig::Setting& log_level_ =
+            kafka_params->lookup("log_level");
 
-    topic = (const char *) topic_;
-    bootstrap_servers = (const char *) bootstrap_servers_;
-    enable_idempotence = (const char *) enable_idempotence_;
-    client_id = (const char *) client_id_;
-    security_protocol = (const char *) security_protocol_;
-    ssl_key_location = (const char *) ssl_key_location_;
-    ssl_certificate_location = (const char *) ssl_certificate_location_;
-    ssl_ca_location = (const char *) ssl_ca_location_;
-    log_level = (const char *) log_level_;
+        this->topic = (const char *) topic_;
+        this->bootstrap_servers = (const char *) bootstrap_servers_;
+        this->enable_idempotence = (const char *) enable_idempotence_;
+        this->client_id = (const char *) client_id_;
+        this->security_protocol = (const char *) security_protocol_;
+        this->ssl_key_location = (const char *) ssl_key_location_;
+        this->ssl_certificate_location = (const char *) ssl_certificate_location_;
+        this->ssl_ca_location = (const char *) ssl_ca_location_;
+        this->log_level = (const char *) log_level_;
+    }   
 }
 
 int KafkaCfgHandler::lookup_kafka_parameters(std::string cfg_path)
--- src/utils/cfg_handler.h
+++ src/utils/cfg_handler.h
@@ -14,43 +14,34 @@ public:
     int lookup_kafka_parameters(std::string cfg_path);
 
     // Getters
-    std::string get_kafka_topic() {
+    const std::string get_kafka_topic() {
                             return topic; };
-    std::string get_kafka_bootstrap_servers() {
+    const std::string get_kafka_bootstrap_servers() {
                             return bootstrap_servers; };
-    std::string get_kafka_enable_idempotence() {
+    const std::string get_kafka_enable_idempotence() {
                             return enable_idempotence; };
-    std::string get_kafka_client_id() {
+    const std::string get_kafka_client_id() {
                             return client_id; };
-    std::string get_kafka_security_protocol() {
+    const std::string get_kafka_security_protocol() {
                             return security_protocol; };
-    std::string get_kafka_ssl_key_location() {
+    const std::string get_kafka_ssl_key_location() {
                             return ssl_key_location; };
-    std::string get_kafka_ssl_certificate_location() {
+    const std::string get_kafka_ssl_certificate_location() {
                             return ssl_certificate_location; };
-    std::string get_kafka_ssl_ca_location() {
+    const std::string get_kafka_ssl_ca_location() {
                             return ssl_ca_location; };
-    std::string get_kafka_log_level() {
+    const std::string get_kafka_log_level() {
                             return log_level; };
 private:
-    //const char *topic = NULL;
-    //const char *bootstrap_servers = NULL;
-    //const char *enable_idempotence = NULL;
-    //const char *client_id = NULL;
-    //const char *security_protocol = NULL;
-    //const char *ssl_key_location = NULL;
-    //const char *ssl_certificate_location = NULL;
-    //const char *ssl_ca_location = NULL;
-    //const char *log_level = NULL;
-    std::string topic;
-    std::string bootstrap_servers;
-    std::string enable_idempotence;
-    std::string client_id;
-    std::string security_protocol;
-    std::string ssl_key_location;
-    std::string ssl_certificate_location;
-    std::string ssl_ca_location;
-    std::string log_level;
+    const std::string topic;
+    const std::string bootstrap_servers;
+    const std::string enable_idempotence;
+    const std::string client_id;
+    const std::string security_protocol;
+    const std::string ssl_key_location;
+    const std::string ssl_certificate_location;
+    const std::string ssl_ca_location;
+    const std::string log_level;
 };
 
 #endif
