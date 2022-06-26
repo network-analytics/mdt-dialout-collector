14edb02ffeed5fe10c0feaebf73335aeb617307e
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-06-26 22:53:56 +0200

    introduce telemetry header proto file (13)...

--- src/mdt_dialout_core.cc
+++ src/mdt_dialout_core.cc
@@ -425,14 +425,15 @@ void Srv::JuniperStream::Start()
         //    std::cout << data.val().any_val().value();
         //}
 
-        auto stream_data_in_ = juniper_stream.update().update();
-        //auto stream_data_in_ = juniper_stream.extension();
+        if (juniper_stream.has_update()) {
+            auto stream_data_in_ = juniper_stream.update().update();
+            //auto stream_data_in_ = juniper_stream.extension();
 
-        int stream_data_in_size_ = stream_data_in_.Capacity();
-        std::cout << stream_data_in_size_ << "\n";
-        //for (auto iter = stream_data_in_.begin(); iter < stream_data_in_.end() and !stream_data_in_.empty(); iter++) {
-        for (auto& iter : stream_data_in_) {
-            std::cout << iter.DebugString().c_str() << "\n";
+            //int stream_data_in_size_ = stream_data_in_.Capacity();
+            //std::cout << stream_data_in_size_ << "\n";
+            //for (auto iter = stream_data_in_.begin(); iter < stream_data_in_.end() and !stream_data_in_.empty(); iter++) {
+            for (auto& iter : stream_data_in_) {
+                std::cout << iter.DebugString().c_str() << "\n";
             //std::cout << iter->registered_ext().msg() << "\n";
             //parsing_str = juniper_tlm_header_ext->ParseFromString(iter->registered_ext().msg());
             //parsing_str = juniper_tlm->ParseFromString(iter->val().any_val().value());
@@ -452,6 +453,7 @@ void Srv::JuniperStream::Start()
             //} else {
             //    std::cout << "Parsing ERROR \n";
             //}
+            }
         }
 
         // handling empty data
