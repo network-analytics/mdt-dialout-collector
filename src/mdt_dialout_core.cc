a8d82b3c212258e94ba6844bef3366b6a391755d
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-06-27 11:53:47 +0200

    introduce telemetry header proto file (15)...

--- src/mdt_dialout_core.cc
+++ src/mdt_dialout_core.cc
@@ -432,20 +432,24 @@ void Srv::JuniperStream::Start()
             //int stream_data_in_size_ = stream_data_in_.Capacity();
             //std::cout << stream_data_in_size_ << "\n";
             //for (auto iter = stream_data_in_.begin(); iter < stream_data_in_.end() and !stream_data_in_.empty(); iter++) {
-            for (const auto& iter : juniper_stream.extension()) {
-                std::cout << iter.registered_ext().msg() << "\n";
+        for (const auto& iter : juniper_stream.extension()) {
+            std::cout << iter.registered_ext().msg() << "\n";
+            if (iter.registered_ext().id() == gnmi_ext::ExtensionID::EID_JUNIPER_TELEMETRY_HEADER) {
                 parsing_str = juniper_tlm_header_ext->ParseFromString(iter.registered_ext().msg());
                 if (parsing_str) {
-                    google::protobuf::util::JsonPrintOptions opt;
-                    opt.add_whitespace = true;
-                    google::protobuf::util::MessageToJsonString(
-                                                        *juniper_tlm_header_ext,
-                                                        &stream_data_in,
-                                                        opt);
-                    std::cout << stream_data_in << "\n";
+                    //google::protobuf::util::JsonPrintOptions opt;
+                    //opt.add_whitespace = true;
+                    //google::protobuf::util::MessageToJsonString(
+                    //                                *juniper_tlm_header_ext,
+                    //                                &stream_data_in,
+                    //                                opt);
+                    //std::cout << stream_data_in << "\n";
+                    std::cout << juniper_tlm_header_ext->streamed_path() << "\n";
                 } else {
                     std::cout << "Parsing ERROR \n";
                 }
+            }
+        }
             //std::cout << iter->registered_ext().msg() << "\n";
             //parsing_str = juniper_tlm_header_ext->ParseFromString(iter->registered_ext().msg());
             //parsing_str = juniper_tlm->ParseFromString(iter->val().any_val().value());
@@ -465,7 +469,6 @@ void Srv::JuniperStream::Start()
             //} else {
             //    std::cout << "Parsing ERROR \n";
             //}
-            }
         //}
 
         // handling empty data
