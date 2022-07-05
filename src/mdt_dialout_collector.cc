be5ece06ead6e11968997d3439e2884a4ad50bb4
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-07-05 18:25:07 +0200

    switching to jthread (5)...

--- src/mdt_dialout_collector.cc
+++ src/mdt_dialout_collector.cc
@@ -6,7 +6,8 @@
 
 void *cisco_thread(void *);
 void *huawei_thread(void *);
-void *juniper_thread(void *);
+//void *juniper_thread(void *);
+int juniper_single_process(void);
 // --- Required for config parameters ---
 std::unique_ptr<MainCfgHandler> main_cfg_handler(new MainCfgHandler());
 // --- Required for config parameters ---
@@ -30,12 +31,19 @@ int main(void)
         vendors.push_back(std::move(cisco_t));
     }
 
+    //if (!(main_cfg_handler->get_ipv4_socket_juniper()).empty()) {
+    //    void *juniper_ptr {nullptr};
+    //    std::thread juniper_t(&juniper_thread, juniper_ptr);
+    //    std::cout << "mdt-dialout-collector listening on "
+    //    << main_cfg_handler->get_ipv4_socket_juniper() << "..." << std::endl;
+    //    vendors.push_back(std::move(juniper_t));
+    //}
+    
     if (!(main_cfg_handler->get_ipv4_socket_juniper()).empty()) {
-        void *juniper_ptr {nullptr};
-        std::thread juniper_t(&juniper_thread, juniper_ptr);
-        std::cout << "mdt-dialout-collector listening on "
-        << main_cfg_handler->get_ipv4_socket_juniper() << "..." << std::endl;
-        vendors.push_back(std::move(juniper_t));
+        if (juniper_single_process() == 0) {
+            std::cout << "mdt-dialout-collector listening on "
+            << main_cfg_handler->get_ipv4_socket_juniper() << "..." << std::endl;
+        }
     }
 
     if (!(main_cfg_handler->get_ipv4_socket_huawei()).empty()) {
@@ -69,7 +77,21 @@ void *cisco_thread(void *cisco_ptr)
     return (EXIT_SUCCESS);
 }
 
-void *juniper_thread(void *juniper_ptr)
+//void *juniper_thread(void *juniper_ptr)
+//{
+//    // --- Required for config parameters ---
+//    std::string ipv4_socket_juniper =
+//        main_cfg_handler->get_ipv4_socket_juniper();
+//    // --- Required for config parameters ---
+//
+//    std::string juniper_srv_socket {ipv4_socket_juniper};
+//    Srv juniper_mdt_dialout_collector;
+//    juniper_mdt_dialout_collector.JuniperBind(juniper_srv_socket);
+//
+//    return (EXIT_SUCCESS);
+//}
+
+int juniper_single_process(void)
 {
     // --- Required for config parameters ---
     std::string ipv4_socket_juniper =
--- src/mdt_dialout_core.cc
+++ src/mdt_dialout_core.cc
@@ -135,13 +135,13 @@ void Srv::JuniperBind(std::string juniper_srv_socket)
     juniper_cq_ = juniper_builder.AddCompletionQueue();
     juniper_server_ = juniper_builder.BuildAndStart();
 
-    std::thread t1(&Srv::JuniperFsmCtrl, this);
-    std::thread t2(&Srv::JuniperFsmCtrl, this);
-    std::thread t3(&Srv::JuniperFsmCtrl, this);
+    //std::thread t1(&Srv::JuniperFsmCtrl, this);
+    //std::thread t2(&Srv::JuniperFsmCtrl, this);
+    //std::thread t3(&Srv::JuniperFsmCtrl, this);
 
-    t1.join();
-    t2.join();
-    t3.join();
+    //t1.join();
+    //t2.join();
+    //t3.join();
 }
 
 void Srv::HuaweiBind(std::string huawei_srv_socket)
