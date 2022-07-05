e51fef7e6b8c0ae242f827d869079fe5319a643e
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-07-05 14:49:49 +0200

    multiple targets sending async Notifications (20)...

--- src/mdt_dialout_core.cc
+++ src/mdt_dialout_core.cc
@@ -136,12 +136,12 @@ void Srv::JuniperBind(std::string juniper_srv_socket)
     juniper_server_ = juniper_builder.BuildAndStart();
 
     std::thread t1(&Srv::JuniperFsmCtrl, this);
-    //std::thread t2(&Srv::JuniperFsmCtrl, this);
-    //std::thread t3(&Srv::JuniperFsmCtrl, this);
+    std::thread t2(&Srv::JuniperFsmCtrl, this);
+    std::thread t3(&Srv::JuniperFsmCtrl, this);
 
     t1.join();
-    //t2.join();
-    //t3.join();
+    t2.join();
+    t3.join();
 }
 
 void Srv::HuaweiBind(std::string huawei_srv_socket)
@@ -407,9 +407,7 @@ void Srv::JuniperStream::Start()
             juniper_tlm_header_ext(new GnmiJuniperTelemetryHeaderExtension());
 
         // the key-word "this" is used as a unique TAG
-//        std::cout << "before reading the jstream from: " << peer << "\n";
-        juniper_resp.Read(&juniper_stream, this);
-//        std::cout << "after reading the jstream from: " << peer << "\n";
+        juniper_resp.Read(juniper_stream, this);
 
         // Decoding the (repeated) extension field
         for (const auto& r_ext : juniper_stream.extension()) {
