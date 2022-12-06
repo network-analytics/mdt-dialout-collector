a1e7a9422a5e859d6487550c90c2c96fb9a8b307
Salvatore Cuzzilla <salvatore@cuzzilla.org>
2022-12-06 11:10:17 +0100

    adding a very simple ZMQ PULLer (2) ...

--- src/pmtelemetryd.c
+++ src/pmtelemetryd.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
     pthread_t *worker = NULL;
     start_grpc_dialout_collector(worker);
 
+<<<<<<< HEAD
     void *ctx = zmq_ctx_new();
     void *zmq_pull = zmq_socket(ctx, ZMQ_PULL);
     zmq_bind(zmq_pull, "ipc:///tmp/grpc.sock");
@@ -27,6 +28,25 @@ int main(int argc, char *argv[])
         FreePayload(pload);
     }
 
+||||||| 434f61c
+=======
+    void *ctx = zmq_ctx_new();
+    void *zmq_pull = zmq_socket(ctx, ZMQ_PULL);
+    zmq_bind(zmq_pull, "ipc:///tmp/grpc.sock");
+    //zmq_bind(zmq_pull, "inproc://grpc");
+
+    while(1) {
+        zmq_recv(zmq_pull, &pload, sizeof(Payload), 0);
+        printf("%s\n", pload->writer_id);
+        printf("%s\n", pload->event_type);
+        printf("%s\n", pload->serialization);
+        printf("%s\n", pload->telemetry_node);
+        printf("%s\n", pload->telemetry_port);
+        printf("%s\n", pload->telemetry_data);
+        FreePayload(pload);
+    }
+
+>>>>>>> zmqpr
     return EXIT_SUCCESS;
 }
 
